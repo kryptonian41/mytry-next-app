54b0064b3ee7a5c8909736b7af7dc3fa97ca6a5d
Apoorv <aapoorv41@gmail.com>
2021-05-29 19:33:51 +0530

    Chore: Added typescript and tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="bg-indigo-800 text-white font-semibold p-4 px-8 flex items-center space-x-4 justify-between">
+      <div className="space-x-6">
+        <span>Home</span>
+        <span>My Account</span>
+      </div>
+
+      <div>
+        <span>Cart</span>
+      </div>
+    </div>
+  )
+}
