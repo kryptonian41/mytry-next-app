b949fb8d0e97587e0c6e2fa54c2fa3669ba7b61d
Apoorv <aapoorv41@gmail.com>
2021-06-09 00:33:02 +0530

    Feat: Added the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,79 @@
-import { getProducts } from 'api-utils'
+import { useTheme } from 'assets/color-map'
+import { HomeProductTile } from 'components/HomeProductTile'
 import { Navbar } from 'components/Navbar'
-import { ProductTile } from 'components/ProductTile'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { reactQueryClient } from 'pages/_app'
-import React, { useEffect, useMemo } from 'react'
-import { useQuery } from 'react-query'
+import React, { useMemo } from 'react'
 import { Product } from 'types/commons'
-import { getProductsServerSide } from './api/products'
 
-const renderProductTiles = (products: Product[]) => {
-  return products.map(product => {
-    return <ProductTile key={product.id} product={product} />
-  })
-}
 
 interface Props {
   products: Product[]
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const { data } = await getProductsServerSide()
-  return {
-    props: {
-      products: data
-    }
-  }
-}
 
-export const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
-  products: productsFromServer
+export const Home: React.FC = ({
 }) => {
-  const { data, isLoading, isError } = useQuery('products', getProducts, {
-    initialData: productsFromServer,
-    staleTime: 1000 * 60
-  })
-
-  useEffect(() => {
-    reactQueryClient.setQueryData('products', productsFromServer)
-  }, [productsFromServer])
+  const theme = useTheme()
 
   const pageBody = useMemo(() => {
-    if (isLoading) return <div>
-      Loading...
+    return <div className="py-16">
+      <h1 className="text-center text-5xl mb-8" style={{
+        color: theme.orange
+      }}>Sustainable, vegan beauty </h1>
+      <p className="text-center text-lg w-2/5 m-auto mb-16" style={{
+        color: theme.green
+      }}>
+        100% Vegan | Responsibly Sourced Ingredients & Materials |
+        Handcrafted + Highly Effective
+      </p>
+
+      <div className="grid grid-cols-2 w-3/6 m-auto gap-24">
+        <div>
+          <HomeProductTile subtitle="EXPLORE CLAY MASKS" title="pore it in
+" imageSrc="/assets/images/about/Group 73.png" />
+        </div>
+        <div className="transform translate-y-10">
+          <HomeProductTile subtitle="EXPLORE CLAY MASKS" title="pore it in
+" imageSrc="/assets/images/about/Group 80.png" textPosition="top" />
+        </div>
+        <div className="col-span-2">
+          <HomeProductTile subtitle="EXPLORE CLAY MASKS" title="pore it in
+" imageSrc="/assets/images/about/Intersection 1@2x.png" />
+        </div>
+      </div>
+
+      <div className="py-20 grid grid-cols-2 border-t border-gray-800 w-3/4 m-auto mt-12">
+        <p className="text-lg" style={{
+          color: theme.green
+        }}>
+
+          <span className="block text-6xl mb-8" style={{
+            color: theme.pink
+          }}>
+            Nicely with <br /> nature
+          </span>
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ad tempora dolorum hic ipsa corrupti consequuntur sequi ipsum, enim, eum ea earum molestias cupiditate culpa voluptas? Voluptatum blanditiis dolores ex modi pariatur tempore illum voluptate veniam ratione temporibus ad praesentium cum accusantium numquam, accusamus at aliquid eligendi! Eveniet accusamus provident tempore sapiente similique quam alias amet neque! Consequuntur, nam quia?
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam rem voluptas cum consequuntur exercitationem quis rerum blanditiis perferendis, cumque tenetur.
+          </p>
+        <div className="pl-12">
+          <img className="m-auto" src="/assets/images/about/Group 74.png" alt="" />
+        </div>
+      </div>
     </div>
 
-    if (isError) return <div>
-      There was some error fetching products list
-    </div>
-
-    if (data) return <div className="pt-6 grid grid-cols-3 w-2/3 m-auto gap-20">
-      {renderProductTiles(data)}
-    </div>
-
-  }, [
-    isLoading, isError, data
-  ])
+  }, [])
 
 
   return (
     <React.Fragment>
-      <Navbar />
+      <div className="h-full relative">
+        <Navbar />
+        <div className="absolute w-full p-4 left-0 bottom-0" style={{
+          background: theme.yellow
+        }}></div>
+      </div>
       {pageBody}
     </React.Fragment>
   )
