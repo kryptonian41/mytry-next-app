9a6101f22f425b8d0a847b3af60ac858e8a92db3
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-06-11 23:36:14 +0530

    removed console logs

--- components/ProductTile.tsx
+++ components/ProductTile.tsx
@@ -10,44 +10,42 @@ interface Props {
 
 const ProductTile: React.FC<Props> = ({ product, addItem }) => {
   return (
-    // <Link href={`/product/${product.slug}`}>
-    //   <a>
-    <div>
-      <div className="px-7">
-        <div className="cursor-pointer relative">
-          {product.images.length > 0 && (
-            <div
-              className="h-80"
-              style={{
-                height: "100%",
+    <Link href={`/product/${product.slug}`}>
+      <a>
+        <div className="px-7">
+          <div className="cursor-pointer relative">
+            {product.images.length > 0 && (
+              <div
+                className="h-80"
+                style={{
+                  height: "100%",
+                }}
+              >
+                <img
+                  src={product.images[0].src}
+                  className="object-cover block w-full h-full"
+                />
+              </div>
+            )}
+            <p className="text-xl font-medium mt-4">{product.name}</p>
+            <p>
+              {RUPEES_SYMBOL} {product.sale_price}
+              <span className="line-through pl-2 text-red-500">
+                {RUPEES_SYMBOL} {product.regular_price}
+              </span>
+            </p>
+            <button
+              onClick={() => {
+                addItem(product);
               }}
+              className="absolute translate-x-1/2 right-0 bottom-28 transform py-1 px-4 bg-pink-800 text-white rounded-full"
             >
-              <img
-                src={product.images[0].src}
-                className="object-cover block w-full h-full"
-              />
-            </div>
-          )}
-          <p className="text-xl font-medium mt-4">{product.name}</p>
-          <p>
-            {RUPEES_SYMBOL} {product.sale_price}
-            <span className="line-through pl-2 text-red-500">
-              {RUPEES_SYMBOL} {product.regular_price}
-            </span>
-          </p>
-          <button
-            onClick={() => {
-              addItem(product);
-            }}
-            className="absolute translate-x-1/2 right-0 bottom-28 transform py-1 px-4 bg-pink-800 text-white rounded-full"
-          >
-            Shop
-          </button>
+              Shop
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
-    // </a>
-    // </Link>
+      </a>
+    </Link>
   );
 };
 
