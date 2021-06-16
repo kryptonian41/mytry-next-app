6d2f0c57ae145fc5680c9da6c6c3beb465f00e38
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-06-16 11:58:08 +0530

    user authenticate, token validate and revoke api endpoints created

--- pages/api/users/authenticate.ts
+++ pages/api/users/authenticate.ts
@@ -1,18 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-export const registerUser = async (body = {}) => {
-  const { firstName, lastName, email, password } = body as any;
+export const authenticateUser = async (body = {}) => {
+  const { email, password } = body as any;
   let response;
   try {
     response = await axios.post(process.env.WCOMM_DOMAIN, null, {
       params: {
-        rest_route: `${process.env.JWT_NAMESPACE}/users`,
+        rest_route: `${process.env.JWT_NAMESPACE}/auth`,
         email,
         password,
-        first_name: firstName,
-        last_name: lastName,
-        AUTH_KEY: process.env.AUTH,
       },
     });
     response.status = 201;
@@ -27,7 +24,7 @@ export const registerUser = async (body = {}) => {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const response = await registerUser(req.body);
+    const response = await authenticateUser(req.body);
     res.status(response.status).json(response.data);
   } else res.status(405).json("");
 };
--- pages/api/users/revoke.ts
+++ pages/api/users/revoke.ts
@@ -1,18 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-export const registerUser = async (body = {}) => {
-  const { firstName, lastName, email, password } = body as any;
+export const revokeToken = async (body = {}) => {
+  const { JWT } = body as any;
   let response;
   try {
     response = await axios.post(process.env.WCOMM_DOMAIN, null, {
       params: {
-        rest_route: `${process.env.JWT_NAMESPACE}/users`,
-        email,
-        password,
-        first_name: firstName,
-        last_name: lastName,
-        AUTH_KEY: process.env.AUTH,
+        rest_route: `${process.env.JWT_NAMESPACE}/auth/revoke`,
+        JWT,
       },
     });
     response.status = 201;
@@ -27,7 +23,7 @@ export const registerUser = async (body = {}) => {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const response = await registerUser(req.body);
+    const response = await revokeToken(req.body);
     res.status(response.status).json(response.data);
   } else res.status(405).json("");
 };
