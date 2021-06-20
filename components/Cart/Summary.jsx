00ba398bc402cf7025ca44561e43925a14731f5b
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-06-20 18:09:38 +0530

    cart page summary complete

--- components/Cart/Summary.jsx
+++ components/Cart/Summary.jsx
@@ -5,6 +5,29 @@ const Summary = () => {
     <div className={cartStyles.summaryContainer}>
       <div className={cartStyles.amountSummary}>
         <h2>Summary</h2>
+        <div className={cartStyles.amountRowsContainer}>
+          <div>
+            <div className={cartStyles.amountRow}>
+              <p>Subtotal</p>
+              <p>INR 0</p>
+            </div>
+          </div>
+          <div>
+            <div className={cartStyles.amountRow}>
+              <p>Shipping</p>
+              <p>INR 0</p>
+            </div>
+            <div className={cartStyles.amountRow}>
+              <p>Taxes</p>
+              <p>INR 0</p>
+            </div>
+            <hr />
+            <div className={cartStyles.amountRow}>
+              <h3>Total</h3>
+              <h3>INR 0</h3>
+            </div>
+          </div>
+        </div>
       </div>
       <button className={cartStyles.checkoutBtn}>Checkout</button>
     </div>
