8ed59b5474e71bfccfb8cb6bcd5c8c4e4b0de4e0
Apoorv <aapoorv41@gmail.com>
2021-06-27 11:58:57 +0530

    Feat: Show discount price conditionally

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -12,10 +12,6 @@ import { getProductsServerSide } from "../api/products";
 import styles from "./styles.module.scss";
 import { Category, processCategories } from "utils";
 
-const renderProductTiles = (products: Product[]) => (
-  <Products products={products} />
-);
-
 interface Props {
   products: Product[];
   parentCategories: Category[];
@@ -68,7 +64,7 @@ export const Home: React.FC<
             parentToChildCategoryMap={parentToChildCategoryMap}
           />
           <div className={styles.productsContainer}>
-            {renderProductTiles(data)}
+            <Products products={data} />
           </div>
         </div>
       );
