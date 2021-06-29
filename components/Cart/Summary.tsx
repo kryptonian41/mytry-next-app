5742e8a22eecb45ce12ccffa3e51f6351942d1f6
Apoorv <aapoorv41@gmail.com>
2021-06-29 17:09:56 +0530

    Fix: Remove unwanted items from the summary when the cart in empty

--- components/Cart/Summary.tsx
+++ components/Cart/Summary.tsx
@@ -1,8 +1,11 @@
 import cartStyles from "./cart.module.scss";
 import clsx from "clsx";
 import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 const Summary = ({ cartTotal, dark = false, buttonComponent = null }) => {
+  const { items } = useSelector((state) => (state as any).cart);
+  const cartFilled = useMemo(() => items.length !== 0, [items])
   return (
     <div className={clsx(cartStyles.summaryContainer, { [cartStyles.dark]: dark })}>
       <div className={cartStyles.amountSummary}>
@@ -15,20 +18,24 @@ const Summary = ({ cartTotal, dark = false, buttonComponent = null }) => {
             </div>
           </div>
           <div>
-            <div className={cartStyles.amountRow}>
+            {cartFilled && <div className={cartStyles.amountRow}>
               <p>Shipping</p>
               <p>INR 50</p>
+            </div>}
+            <div className={cartStyles.amountRow}>
+              <p>Payment Mode</p>
+              <p>COD</p>
             </div>
 
             <hr />
             <div className={cartStyles.amountRow}>
               <h3>Total</h3>
-              <h3>INR {cartTotal + 50}</h3>
+              <h3>INR {cartTotal + (cartFilled ? 50 : 0)}</h3>
             </div>
           </div>
         </div>
       </div>
-      {buttonComponent && <div className="mt-5">
+      {cartFilled && buttonComponent && <div className="mt-5">
         {buttonComponent}
       </div>}
     </div>
