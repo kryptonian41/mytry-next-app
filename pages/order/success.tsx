e1aeb85f0fb34db6ba30d3f37e248e582944ebef
Apoorv <aapoorv41@gmail.com>
2021-06-29 11:05:41 +0530

    FIx: The order success page

--- pages/order/success.tsx
+++ pages/order/success.tsx
@@ -1,27 +1,35 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import Navbar from 'components/Navbar'
 import { useTheme } from 'utils/color-map'
+import { isServer } from 'utils'
 interface Props {
 
 }
 
 const OrderSuccess = (props: Props) => {
-  const { query } = useRouter()
   const theme = useTheme()
+  const [isMobile, setIsMobile] = useState(() => {
+    return null
+  })
+
+  useLayoutEffect(() => {
+    setIsMobile(window.matchMedia('(max-width: 640px)'))
+  }, [])
+
   return (
     <div className="h-full flex flex-col" style={{
       backgroundColor: theme.green
     }}>
       <Navbar color="light" />
-      <div className="min-h-0 flex-1 grid place-items-center">
+      <div className="min-h-0 flex-1 grid place-items-center w-full">
         <div style={{
-          width: 550
+          width: isMobile ? '80%' : 550
         }}>
           <h1 style={{
             color: theme.yellow
-          }} className="text-center text-5xl">Your order has been placed !</h1>
-          <p className="text-white text-2xl text-center mt-4">Shipping details will be in your inbox</p>
+          }} className="text-center text-2xl sm:text-5xl">Your order has been placed !</h1>
+          <p className="text-white sm:text-2xl text-center mt-4">Shipping details will be in your inbox</p>
         </div>
       </div>
     </div>
