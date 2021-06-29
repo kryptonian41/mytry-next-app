bb72157a13704b371866759f2de25344c6eba556
Apoorv <aapoorv41@gmail.com>
2021-06-29 11:05:56 +0530

    Use razorpay client for verification

--- pages/api/order/verification.ts
+++ pages/api/order/verification.ts
@@ -1,13 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { wooClient } from "utils/api-utils";
-import crypto from 'crypto'
 import { razorpayClient } from "utils/api-utils/razorpay-client";
-
-const createHmacDigest = (body: any, secret: string) => {
-  const hmac = crypto.createHmac('sha256', secret)
-  hmac.update(JSON.stringify(body))
-  return hmac.digest('hex')
-}
 
 const updateOrder = async (orderId: string, updateData: any) => {
   const { data } = await wooClient.put(`orders/${orderId}`, updateData)
@@ -16,9 +9,14 @@ const updateOrder = async (orderId: string, updateData: any) => {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const hmacDigest = createHmacDigest(req.body, process.env.RAZORPAY_WEBHOOK_SECRET)
     try {
-      if (req.headers['x-razorpay-signature'] === hmacDigest) {
+      const body = JSON.stringify(req.body),
+        signature = req.headers['x-razorpay-signature'],
+        secret = process.env.RAZORPAY_WEBHOOK_SECRET
+
+      const validSignature = razorpayClient.validateWebhookSignature(body, signature, secret)
+
+      if (validSignature) {
         const { entity: { order_id } } = req.body.payload.payment
         const rzpOrderDetails = await razorpayClient.orders.fetch(order_id)
         const orderID = rzpOrderDetails.receipt
@@ -26,6 +24,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           status: 'processing',
           transaction_id: order_id
         })
+      } else {
+        console.log('Payment Denied')
       }
     } catch (error) {
       console.log("🚀 ~ file: verification.ts ~ line 28 ~ error", error)
