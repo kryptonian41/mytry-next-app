61e190faa3f7928b1fbe5c78fede3c8f95eacd39
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-03 05:06:02 +0530

    cart price fix on application load

--- utils/cart/index.ts
+++ utils/cart/index.ts
@@ -1,32 +1,35 @@
 import { Product } from "types/commons";
 
 export interface CartProduct {
-  id: number,
-  name: string,
-  image: string,
-  price: number,
-  descQty: number,
-  qty: number,
-  totalPrice: number,
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  descQty: number;
+  qty: number;
+  totalPrice: number;
 }
 
-export const createCartState = (products: Product[], qtyMap: Map<number, number>) => {
-  const cartItems = products.map(product => {
-    const productQuantity = qtyMap.get(product.id)
+export const createCartState = (
+  products: Product[],
+  qtyMap: Map<number, number>
+) => {
+  const cartItems = products.map((product) => {
+    const productQuantity = qtyMap.get(product.id);
     return {
       id: product.id,
       name: product.name,
       image: product.images[0].src,
       price: parseFloat(product.price),
       qty: productQuantity,
-      totalPrice: parseInt(product.price) * productQuantity,
-    }
-  })
-  const totalQty = cartItems.reduce((acc, item) => acc + item.qty, 0)
-  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0)
+      totalPrice: parseFloat((parseFloat(product.price) * productQuantity).toFixed(2)),
+    };
+  });
+  const totalQty = cartItems.reduce((acc, item) => acc + item.qty, 0);
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.totalPrice, 0);
   return {
     items: cartItems,
     itemsCount: totalQty,
-    cartTotal: totalPrice
-  }
-}
+    cartTotal: totalPrice,
+  };
+};
