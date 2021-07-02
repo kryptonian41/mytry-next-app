0ecaf8b19d0ed955a1fe6c6b606019a01e1a02ae
Apoorv <aapoorv41@gmail.com>
2021-07-02 13:16:57 +0530

    Feat: Update the contact us page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -16,26 +16,48 @@ export const ProductPage = () => {
       >
         <div className={styles.hero}>
           <Navbar color="light" className="z-10" />
-          <div className={clsx(styles["hero-contact-wrapper"], "z-0 ")}>
+          <div className={clsx(styles["hero-contact-wrapper"], "z-0")}>
             <div
               className={clsx(
-                "mt-8 sm:mt-0 py-6 pl-12 order-2 sm:order-none",
+                "mt-8 sm:mt-0 py-6 pt-16 sm:pt-6 pl-6 pr-12 sm:pr-0 sm:pl-12 order-2 sm:order-none",
                 styles["hero-contact-info"]
               )}
               style={{
                 backgroundColor: theme.orange,
               }}
             >
-              <p className="text-white uppercase sm:text-lg mb-1">
+              <p className="text-white uppercase sm:text-2xl mb-4">
                 General Inquiries
               </p>
+              <p className="text-white uppercase sm:text-lg mb-1">
+                Contact No.
+              </p>
               <p
-                className="uppercase sm:text-2xl"
+                className="uppercase sm:text-xl mb-4"
                 style={{
                   color: theme.yellow,
                 }}
               >
-                hello@mytryshop.com
+                +91 6359 874 699
+              </p>
+              <p className="text-white uppercase sm:text-lg mb-1">
+                Address
+              </p>
+              <p
+                className="uppercase sm:text-xl"
+                style={{
+                  color: theme.yellow,
+                }}
+              >
+                703-4 C Tower Happy Residency
+                <br />
+                Opp. Safal Square Vesu
+                <br />
+                Surat - 395007
+                <br />
+                Gujarat
+                <br />
+                India
               </p>
             </div>
 
