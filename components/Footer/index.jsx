8d72eb5d27c87fd07cbbfc9f1653bddad7e3c348
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-13 01:00:43 +0530

    added link to footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,17 +1,29 @@
 import footerStyles from "./footer.module.scss";
 import Logo from "assets/svgs/logos/footer-logo.svg";
+import Link from "next/link";
 
 const index = () => {
   return (
-    <footer className={footerStyles.footerContainer}>
-      <h3>For daily goodness</h3>
-      <form>
+    <footer>
+      <div className={footerStyles.footerContainer}>
+        <h3>For daily goodness</h3>
+        <form>
           <input type="text" placeholder="EMAIL" />
           <br />
           <button type="submit">subscribe</button>
-      </form>
-      <div className={footerStyles.logoContainer}>
-        <Logo />
+        </form>
+        <div className={footerStyles.logoContainer}>
+          <Logo />
+        </div>
+      </div>
+      <div className={footerStyles.linksContainer}>
+        <Link href="/terms-and-conditions">Terms & Conditions</Link>
+        <span className={footerStyles.linkDivider}>|</span>
+        <Link href="/privacy-policy">Privacy Policy</Link>
+        <span className={footerStyles.linkDivider}>|</span>
+        <Link href="/shipping-policy">Shipping Policy</Link>
+        <span className={footerStyles.linkDivider}>|</span>
+        <Link href="/returns-and-refund-policy">Returns and Refund</Link>
       </div>
     </footer>
   );
