436048ed79656a6113ccf2072a622910a9044089
Apoorv Agarwal <aapoorv41@gmail.com>
2025-02-24 20:07:29 +0530

    chore: add .env to .gitignore

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,56 +1,64 @@
-import { useMemo } from 'react'
-import ProductTile from './ProductTile'
-import { connect } from 'react-redux'
-import styles from 'pages/products/styles.module.scss'
-import { RootState } from 'redux-state/store'
+import { useMemo } from "react";
+import ProductTile from "./ProductTile";
+import { connect } from "react-redux";
+import styles from "pages/products/styles.module.scss";
+import { RootState } from "redux-state/store";
+import { Category, Product } from "types";
 
-const checkProductBelongsToCategory = (productCategories, categoryId) => {
-  return productCategories.map((category) => category.id).includes(categoryId)
-}
+const checkProductBelongsToCategory = (
+  productCategories: Category[],
+  categoryId: number
+) => {
+  return productCategories.map((category) => category.id).includes(categoryId);
+};
 
-const sortAToZ = (products) => {
+const sortAToZ = (products: Product[]) => {
   return products.sort((a, b) =>
     a.name.toLowerCase() > b.name.toLowerCase()
       ? 1
       : b.name.toLowerCase() > a.name.toLowerCase()
       ? -1
       : 0
-  )
-}
+  );
+};
 
-const sortPrice = (products, isDescending = false) => {
+const sortPrice = (products: Product[], isDescending = false) => {
   const sortedProducts = products.sort((a, b) =>
     parseFloat(a.price) > parseFloat(b.price)
       ? 1
       : parseFloat(b.price) > parseFloat(a.price)
       ? -1
       : 0
-  )
-  if (isDescending) return sortedProducts.reverse()
-  return sortedProducts
-}
+  );
+  if (isDescending) return sortedProducts.reverse();
+  return sortedProducts;
+};
 
-const Products = ({ products, categoryId, sorting }) => {
+const Products: React.FC<{
+  products: Product[];
+  categoryId?: number;
+  sorting?: string;
+}> = ({ products, categoryId, sorting }) => {
   const filteredProductsArray = useMemo(() => {
-    if (!categoryId && !sorting) return products
-    let updatedProductsArray = products
+    if (!categoryId && !sorting) return products;
+    let updatedProductsArray = products;
     if (categoryId) {
       updatedProductsArray = products.filter((product) =>
         checkProductBelongsToCategory(product.categories, categoryId)
-      )
+      );
     }
     if (sorting) {
-      if (sorting === 'atoz')
-        updatedProductsArray = sortAToZ(updatedProductsArray)
-      else if (sorting === 'priceAscending' || sorting === 'priceDescending') {
+      if (sorting === "atoz")
+        updatedProductsArray = sortAToZ(updatedProductsArray);
+      else if (sorting === "priceAscending" || sorting === "priceDescending") {
         updatedProductsArray = sortPrice(
           updatedProductsArray,
-          sorting === 'priceDescending'
-        )
+          sorting === "priceDescending"
+        );
       } else;
     }
-    return updatedProductsArray
-  }, [products, categoryId, sorting])
+    return updatedProductsArray;
+  }, [products, categoryId, sorting]);
 
   return filteredProductsArray.map((product, i) => {
     return (
@@ -60,13 +68,13 @@ const Products = ({ products, categoryId, sorting }) => {
       >
         <ProductTile product={product} />
       </div>
-    )
-  })
-}
+    );
+  });
+};
 
 const mapStateToProps = (state: RootState) => ({
   categoryId: state.productFilters.categoryId,
   sorting: state.productFilters.sorting,
-})
+});
 
-export default connect(mapStateToProps)(Products)
+export default connect(mapStateToProps)(Products);
